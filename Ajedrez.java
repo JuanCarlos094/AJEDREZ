22427fa04b5bbee7be9df8eaf6f82a13447565f9
Juan Carlos <juancarlos.rabasco@gmail.com>
2022-12-12 11:02:39 +0100

    Elegir pieza

--- Ajedrez.java
+++ Ajedrez.java
@@ -1,36 +1,42 @@
 import java.util.Scanner;
-
 public class Ajedrez {
     public static void main(String[]args){
+        String pieza = setPieza();
+        compararPiezas(pieza);
 
-        System.out.println("Bienvenido al juego del Ajedrez.");
-        setPieza();
-        setColor();
+
     }
-    private static String[][] tablero=new String[8][8];
-    private static Scanner sc=new Scanner(System.in);
-    private static String color;
-    private static boolean blancas=false;
-    private static boolean negras=false;
 
     public static String setPieza(){
-
-        System.out.println("En primer lugar, elige la pieza que deseas.");
-        return sc.nextLine();
+        String p;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Elige una pieza a poner en el tablero: ");
+        return p = sc.next();
     }
 
-    public static boolean setColor(){
+    public static void compararPiezas(String pieza){
+        String[] allpieces = {"peon", "caballo", "alfil", "torre", "reina", "rey"};
 
-        System.out.println("En primer lugar, elige el color de la pieza que quieras jugar. Blancas/Negras.");
-        color=sc.nextLine();
+        if (pieza.equalsIgnoreCase(allpieces[0])){
+                //Aqui se llamaria al metodo que corresponde a la pieza con la que coincide
+        }else if (pieza.equalsIgnoreCase(allpieces[1])){
+            //Aqui se llamaria al metodo que corresponde a la pieza con la que coincide
+        }else if (pieza.equalsIgnoreCase(allpieces[2])){
+            //Aqui se llamaria al metodo que corresponde a la pieza con la que coincide
+        }else if (pieza.equalsIgnoreCase(allpieces[3])){
+            //Aqui se llamaria al metodo que corresponde a la pieza con la que coincide
+        }else if (pieza.equalsIgnoreCase(allpieces[4])){
+            //Aqui se llamaria al metodo que corresponde a la pieza con la que coincide
+        }else if (pieza.equalsIgnoreCase(allpieces[5])){
+            //Aqui se llamaria al metodo que corresponde a la pieza con la que coincide
+        }else{
+            System.out.println("Pieza no valida");
+        }
 
-        if(color.equalsIgnoreCase("Blancas")){
-            return blancas=true;
-        }
-        else if(color.equalsIgnoreCase("Negras")){
-            return negras=true;
-        }
-        return false;
+
     }
+
+
+
 
 }
